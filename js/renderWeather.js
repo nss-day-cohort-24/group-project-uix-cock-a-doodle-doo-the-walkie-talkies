6c005b5df16dc219a20d41b609c7aed963d4e84b
Sandhya Rao <smudra@yahoo.com>
2018-03-06 16:11:22 -0600

    the temperature is showing based on zip

--- js/renderWeather.js
+++ js/renderWeather.js
@@ -7,7 +7,7 @@ var showWeatherData = document.getElementById("weather");
 
 function getWeather() {
     return $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?lat=36&lon=86`
+        url: `http://api.openweathermap.org/data/2.5/weather?zip=37211,us&units=imperial&appid=f003b59e8e7d506d9b387f115f63483e`
     }).done((weatherData) => {
         console.log("show me weatherData: ", weatherData);
         return weatherData;
@@ -19,14 +19,14 @@ function getWeather() {
 var currentWeather;
 
 function showWeather() {
-getWeather().then((weatherData) => {
- currentWeather = weatherData.dt;
-console.log("WeatherData", weatherData);
+        getWeather().then((weatherData) => {
+        currentWeather = weatherData.main.temp;
+        console.log("WeatherData", weatherData);
 
+        showWeatherData.innerHTML = `<p>${currentWeather}</p>`;
     });
 }
-showWeatherData.innerHTML = showWeather();
 
-
+showWeather();
 
 module.exports = {showWeather};
