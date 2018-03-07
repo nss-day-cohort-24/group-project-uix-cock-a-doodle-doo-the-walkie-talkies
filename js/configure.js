e67ddf1499fcf42a1ca70a78fcecf1edf5d269ef
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-03-06 20:08:21 -0600

    touched a lot of files to make this login work. got jquery running and edited gruntfile accordingly.

--- js/configure.js
+++ js/configure.js
@@ -1,21 +1,27 @@
 "use strict";
+console.log("configure file for Firebase");
 
+// REQUIRES
+var firebase = require("firebase/app");
+    require("firebase/auth");
+    require("firebase/database");
 
+let fbKey = require("./firebaseKey"),
+    fbData = fbKey();
 
-// var config = {
-//     apiKey: fbData.apiKey,
-//     authDomain: fbData.authDomain,
-//     databaseURL: fbData.databaseURL
-//   };
+var config = {
+    apiKey: fbData.apiKey,
+    authDomain: fbData.authDomain,
+    databaseURL: fbData.databaseURL
+  };
   
   
-//   firebase.getFBsettings = function(){
-//        console.log("getFBsettings", config);
-//        return config;
-//   };
+  firebase.getFBsettings = function(){
+       console.log("getFBsettings", config);
+       return config;
+  };
   
-//   firebase.initializeApp(config);
-//   module.exports = firebase;
+  firebase.initializeApp(config);
   
 
-//   module.exports = {};
+  module.exports = {firebase};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 "use strict";
-
-
-let renderDOM = require('./renderDOM');
-
-
 console.log("main.js is here");
+
+let $ = require('jquery'),
+    renderDOM = require('./renderDOM'),
+    login = require('./user');
+
+$("#login").click(function(){
+    console.log("user clicked login");
+    login.googleLogIn()
+    .then((result) => {
+        console.log("UID result from login: ", result.user.uid);
+        login.setUser(result.user.uid);
+        console.log("login complete!");
+    });
+});
+
+
