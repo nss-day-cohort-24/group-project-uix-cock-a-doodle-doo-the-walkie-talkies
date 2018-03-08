16ba0d5ffa9afa17b1dffa152d4f4a2a2bddb85d
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-03-08 10:59:08 -0600

    starting search

--- js/searchBooks.js
+++ js/searchBooks.js
@@ -1,38 +1,38 @@
-// "use strict";
+"use strict";
 
-// console.log("search books js is here");
+console.log("search books js is here");
 
-// "use strict";
-// let $ = require('jquery'),
-// firebase = require("./configure"),
-// userData = require("./user");
-
-
-// var bookInput = document.getElementById("bookSearch").val;
-// var bookResults = document.getElementById("bookResults");
-
-// //get weather information from API
-// function getBooks() {
-//     return $.ajax({
-//         url: `http://openlibrary.org/search.json?q='${bookInput}`
-//     }).done((bookReturn) => {
-//         console.log("Book search results", bookReturn);
-//         return bookReturn;
-//     });
-// }
-
-// // building DOM for books search results
-// function showBooksResults() {
-//     getBooks().then((bookReturn) => {
-//     $("#primaryContainer").HTML = `<p>${currentWeather}</p><p>${weatherDetails}</p>`;
-
-//     console.log("What are the books results?", bookReturn);
-// });
-// }
+"use strict";
+let $ = require('jquery'),
+firebase = require("./configure"),
+userData = require("./user");
 
 
+var bookInput = document.getElementById("bookSearch").val;
+var bookResults = document.getElementById("bookResults");
 
-// module.exports = {
-//     showBooksResults,
-//     addingZip
-// };
+//get weather information from API
+function getBooks() {
+    return $.ajax({
+        url: `http://openlibrary.org/search.json?q='${bookInput}`
+    }).done((bookReturn) => {
+        console.log("Book search results", bookReturn);
+        return bookReturn;
+    });
+}
+
+// building DOM for books search results
+function showBooksResults() {
+    getBooks().then((bookReturn) => {
+    $("#primaryContainer").HTML = `<p>${currentWeather}</p><p>${weatherDetails}</p>`;
+
+    console.log("What are the books results?", bookReturn);
+});
+}
+
+
+
+module.exports = {
+    showBooksResults,
+    addingZip
+};
