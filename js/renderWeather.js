8d670f6bc98dc128fd4358be46db18df4181f0f4
Sandhya Rao <smudra@yahoo.com>
2018-03-08 18:46:09 -0600

    added POST FB function

--- js/renderWeather.js
+++ js/renderWeather.js
@@ -2,9 +2,9 @@
 console.log("renderWeather file is coming through");
 let $ = require('../lib/node_modules/jquery'),
 firebase = require("./configure"),
-userData = require("./user");
+userData = require("./user"),
+todayDate = require("./dateToday");
 
-// &apiKey=f003b59e8e7d506d9b387f115f63483e
 
 var showWeatherData = document.getElementById("weatherBlock");
 
@@ -13,16 +13,12 @@ function getWeather() {
     return $.ajax({
         url: `http://api.openweathermap.org/data/2.5/weather?zip=37211,us&units=imperial&appid=f003b59e8e7d506d9b387f115f63483e`
     }).done((weatherData) => {
-        console.log("show me weatherData: ", weatherData);
         return weatherData;
-    });
+    });   
 }
 
-//add zipcode information to Firebase. Since this info need to be overwritten we use PUT and not POST. POST is for appending not overwriting.
+//add zipcode information to Firebase. Since this info need to be overwritten we use PUT and not POST. POST is for appending not overwriting. PATCH will overwrite only one key.
 function addingZip(captureZip) {
-    console.log("what's in captureZip inside addingZip ", captureZip);
-    // let test = `${firebase.getFBsettings().databaseURL}/userInfo/-L74tKwYPWEo1O8zgtZ1.json`;
-    // console.log("url", test);
     
     return $.ajax({
         url: `${firebase.getFBsettings().databaseURL}/userInfo/-L75FYIvED1DNEb40InK.json`,
@@ -30,16 +26,9 @@ function addingZip(captureZip) {
         data: JSON.stringify(captureZip),
         dataType: 'json'
     }).done((zipData) => {
-        console.log("show me zipData: ", zipData);
         return zipData;
     });
 }
-
-// Get weather 
-function addWeatherFB() {
-
-}
-
 
 var currentWeather;
 var weatherDetails;
@@ -50,14 +39,43 @@ function showWeather() {
         currentWeather = weatherData.main.temp.toFixed(0) + ' &#8457;';
         weatherDetails = weatherData.weather[0].icon + " " + weatherData.weather[0].description + `<div id="zip-code"></div>`;// weather details like sunny, partly cloudy
 
-        console.log("WeatherData", weatherData);
-        console.log("weather Details", weatherDetails);
-
         showWeatherData.innerHTML = `<p>${currentWeather}</p><p>${weatherDetails}</p>`;
 
+        // addingWeatherFB(tacoWeather);
+        // addWeatherFB(); 
         inputZipcode();
-        console.log("What's showweatherData showing", showWeatherData);
     });
+}
+
+// Add weather to FB
+function addingWeatherFB(tacoWeather) {
+    return $.ajax({
+        url: `${firebase.getFBsettings().databaseURL}/currentWeather.json`,
+        type: 'POST',
+        data: JSON.stringify(tacoWeather),
+        dataType: 'json'
+    }).done((fbWeatherData) => {
+        return fbWeatherData;
+    });
+}
+
+let loadWeather = (currentWeather, weatherDetails) => {
+    let tempLoad = {
+        temp: currentWeather,
+        detail: weatherDetails
+    };
+return tempLoad;
+};
+
+
+var detailLoad;
+function addWeatherFB(){);
+    // getWeather().then((nashData) => {
+    //     let wetObject = loadWeather(nashData.list[0].main.temp, nashData.list[0].weather[0].description);
+    //     addingWeatherFB(wetObject);
+    //     console.log("Whats in the addWeatherFb", wetObject);
+
+    // });
 }
 
 
@@ -72,17 +90,20 @@ var enterZip = document.getElementById("enterZip");
 function inputZipcode(){
 
     enterZip.addEventListener("click", function() {
-        var inputuserZip = userZip.value; // using zipcode value entered by user        
-        console.log("What's in inputuserZip ", inputuserZip);
+        getWeather().then((nashData) => {
+            let wetObject = loadWeather(nashData.list[0].main.temp, nashData.list[0].weather[0].description);
+            addingWeatherFB(wetObject);
+
+        });
+    
+        var inputuserZip = userZip.value; // using zipcode value entered by user 
         // creating zip key in FB
         
         captureUserZip = {
             zipCode: inputuserZip
         }; 
         addingZip(captureUserZip).then((data) => {
-            console.log("What's in data ", data);
         });
-
     });
 }
 
